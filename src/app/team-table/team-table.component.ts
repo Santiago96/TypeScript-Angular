1ceaf691ec133193a77a2872d7a0403a598ad141
Santiago Collazos Barrera <santicollazosbarrera@hotmail.com>
2020-02-10 23:28:29 -0500

    Adding component's logic to retrieve data from DB and show in a table

--- src/app/team-table/team-table.component.ts
+++ src/app/team-table/team-table.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { TeamService } from '../services/team.service';
+import { TeamsTableHeaders } from '../services/team.service';
+import { Team } from '../interfaces/team';
+import { Observable } from 'rxjs';
+import { take } from 'rxjs/operators';
+import { Countries } from '../enums/Countries';
 
 @Component({
   selector: 'app-team-table',
@@ -7,9 +13,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TeamTableComponent implements OnInit {
 
-  constructor() { }
+  public teams$ = Observable<Team[]>;
+  public tableHeaders = TeamsTableHeaders;
+  
+  constructor(private teamService: TeamService) { }
 
   ngOnInit(): void {
+    this.teams$ = this.teamService.getTeams();
+    this.teamService.getTeams().pipe(take(1)).subscribe(teams => {
+      if(teams.length == 0) {
+        const team: Team = {
+          name: 'MyAmazingTeam',
+          country: Countries.Argentina,
+          players: null,          
+        };
+        this.teamService.addTeam(team);
+      }
+    });
   }
 
 }
